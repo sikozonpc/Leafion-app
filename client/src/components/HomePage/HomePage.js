67c45dec15ba88f7c7a77e9df14c55c1e914470b
sikozonpc <sikozonpc@gmail.com>
2019-03-21 21:54:35 +0000

    home page redone

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,27 +1,91 @@
 import React from 'react';
+
 import MonthItem from "./MonthItem/MonthItem";
 
 import classes from "./HomePage.module.css";
 
-import { Container, Row, Col } from "react-bootstrap";
+import { VictoryChart, VictoryLine, VictoryTheme } from "victory";
+
+import { Container, Row, Col, Alert } from "react-bootstrap";
 
 
 const homePage = (props) => {
-    console.log("[HomePage.js] @render | props: " , props.data);
+    console.log("[HomePage.js] @render | props: " , props);
+
+    const recebidoGraphFormatedData = {};
+    const gastoGraphFormatedData = {};
+    //TODO: Add another line in the graph for the expect
+    Object.keys(props.months).map( m => {
+        gastoGraphFormatedData[ m.slice(0,3) ] = getMoneyFromMonth(props.data, props.months[m], "g");
+    });
+    Object.keys(props.months).map( m => {
+        recebidoGraphFormatedData[ m.slice(0,3) ] = getMoneyFromMonth(props.data, props.months[m], "r");
+    });
 
 
     return (
-        <Container style={{background: "#f0f0f0"}}>
-        <Row>
-            {Object.keys(props.months).map(m => {
-                return  <MonthItem 
-                    key={m}
-                    name={m}
-                    totalRecebido={getMoneyFromMonth(props.data, props.months[m], "r")} 
-                    totalGasto={getMoneyFromMonth(props.data, props.months[m], "g")} />
-            })}
-        </Row>
+        <Container>
+            <Alert variant="success">Your financial situation seems pretty stable! Congratz !!</Alert>
+            <Container className={classes.IntroBox}>
+            <h5>Hey <strong>Tiago</strong>, this is your financial summary for the {new Date().getFullYear()} year.</h5>
+            <Row>
+                <Col>
+                    <VictoryChart 
+                        theme={VictoryTheme.material}>
+                        <VictoryLine
+                            style={{
+                            data: { stroke: "#29963d" },
+                            parent: { border: "1px solid #ccc" }
+                            }}
+                            data={[
+                            { x: "Jan", y: recebidoGraphFormatedData["Jan"] },
+                            { x: "Feb", y: recebidoGraphFormatedData["Feb"] },
+                            { x: "Mar", y: recebidoGraphFormatedData["Mar"] },
+                            { x: "Apr", y: recebidoGraphFormatedData["Apr"] },
+                            { x: "May", y: recebidoGraphFormatedData["May"] },
+                            { x: "Jun", y: recebidoGraphFormatedData["Jun"] },
+                            { x: "Jul", y: recebidoGraphFormatedData["Jul"] },
+                            { x: "Aug", y: recebidoGraphFormatedData["Aug"] },
+                            { x: "Sep", y: recebidoGraphFormatedData["Sep"] },
+                            { x: "Oct", y: recebidoGraphFormatedData["Oct"] },
+                            { x: "Nov", y: recebidoGraphFormatedData["Nov"] },
+                            { x: "Dec", y: recebidoGraphFormatedData["Dec"] }
+                            ]}
+                        />
+                        <VictoryLine  height={400} width={400}
+                        style={{
+                        data: { stroke: "red" },
+                        parent: { border: "1px solid #ccc" }
+                        }}
+                        data={[
+                            { x: "Jan", y: gastoGraphFormatedData["Jan"] },
+                            { x: "Feb", y: gastoGraphFormatedData["Feb"] },
+                            { x: "Mar", y: gastoGraphFormatedData["Mar"] },
+                            { x: "Apr", y: gastoGraphFormatedData["Apr"] },
+                            { x: "May", y: gastoGraphFormatedData["May"] },
+                            { x: "Jun", y: gastoGraphFormatedData["Jun"] },
+                            { x: "Jul", y: gastoGraphFormatedData["Jul"] },
+                            { x: "Aug", y: gastoGraphFormatedData["Aug"] },
+                            { x: "Sep", y: gastoGraphFormatedData["Sep"] },
+                            { x: "Oct", y: gastoGraphFormatedData["Oct"] },
+                            { x: "Nov", y: gastoGraphFormatedData["Nov"] },
+                            { x: "Dec", y: gastoGraphFormatedData["Dec"] }
+                            ]}
+                    />
+                    </VictoryChart>
+                </Col>
+                <Col style={{textAlign: "left", marginTop: "50px"}}>
+                    <h3>Statistics</h3>
+                    <p>Total money earned: </p>
+                    <p>Total money spent: </p>
+                </Col>
+            </Row>
+            </Container>
+            <Container className={classes.IntroBox}>
+                <p>...</p>
+            </Container>
         </Container>
+        
     );
 };
 
