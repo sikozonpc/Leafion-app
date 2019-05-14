cbdd5e207208ea9fcf87adb3379bb6813ee984db
Tiago Taquelim <sikozonbatata@gmail.com>
2019-05-14 16:05:08 +0100

    redux not visable in prod

--- client/src/index.js
+++ client/src/index.js
@@ -19,7 +19,7 @@ const rootReducer = combineReducers({
 });
 
 // Middleware
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = process.env.NODE_ENV === "development" ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__: null  || compose  ;
 
 // Setting up the Redux Store
 const store = createStore(
